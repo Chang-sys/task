baff0d8a080df40b9b91a97d3ee31b415bf1ebba
Chhengchang Ngor <chhengchang@sala.co>
2024-10-21 14:42:53 +0700

    refector: :recycle: update on router with hash and configure deploy in dist

--- dist/assets/store.6aec2646.js
+++
@@ -1,1 +0,0 @@
-import{s as r,c as t,u as e}from"./index.cf3c39f6.js";var a=r(function(){return t({modules:{user:e},strict:!1})});export{a as default};
--- dist/assets/store.afd40230.js
+++
@@ -1,1 +0,0 @@
-import{s as r,c as t,u as e}from"./index.4969bf4c.js";var a=r(function(){return t({modules:{user:e},strict:!1})});export{a as default};
